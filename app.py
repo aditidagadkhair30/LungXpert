5b99195a109a7c53714d21275e003189edaca16b
Aditya Desity <aditya.desity20@pccoepune.org>
2024-02-14 19:47:21 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,125 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+import cv2
+import pickle
+import imutils
+import sklearn
+from tensorflow.keras.models import load_model
+# from pushbullet import PushBullet
+import joblib
+import numpy as np
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+
+# Loading Model
+pneumonia_model = load_model('models/pneumonia_model_resnet101.h5')
+covid_model = load_model('models\covid.h5')
+
+# Configuring Flask
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+########################### Routing Functions ########################################
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/pneumonia')
+def pneumonia():
+    return render_template('pneumonia.html')
+
+@app.route('/covid')
+def covid():
+    return render_template('covid.html')
+
+
+########################### Result Functions ########################################
+
+
+@app.route('/resultp', methods=['POST'])
+def resultp():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        age = request.form['age']
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Image successfully uploaded and displayed below')
+            img = cv2.imread('static/uploads/'+filename)
+            img = cv2.resize(img,(224,224))
+            img = np.expand_dims(img,axis=0)
+            img = img/255.0
+            pred = pneumonia_model.predict(img)
+            if pred < 0.5:
+                pred = 0
+            else:
+                pred = 1
+            # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour COVID-19 test results are ready.\nRESULT: {}'.format(firstname,['POSITIVE','NEGATIVE'][pred]))
+            return render_template('resultp.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+        else:
+            flash('Allowed image types are - png, jpg, jpeg')
+            return redirect('/')
+@app.route('/resultc', methods=['POST'])
+def resultc():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        age = request.form['age']
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Image successfully uploaded and displayed below')
+            img = cv2.imread('static/uploads/'+filename)
+            img = cv2.resize(img, (224, 224))
+            img = img.reshape(1, 224, 224, 3)
+            img = img/255.0
+            pred = covid_model.predict(img)
+            if pred < 0.5:
+                pred = 0
+            else:
+                pred = 1
+            # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour COVID-19 test results are ready.\nRESULT: {}'.format(firstname,['POSITIVE','NEGATIVE'][pred]))
+            return render_template('resultc.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+        else:
+            flash('Allowed image types are - png, jpg, jpeg')
+            return redirect(request.url)
+
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
